929148d7e9c4bfbadda63102196e706f97b49876
= <=>
2021-05-18 17:56:47 +0300

    login-in (base)

---
+++ Assets/Scripts/DbManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityNpgsql;
+
+public class DbManager : MonoBehaviour
+{
+    #region дані для конекту
+
+    public string ipAdress;
+    public string port;
+    public string login;
+    public string password;
+    public string dbName;
+
+    #endregion
+
+    private NpgsqlConnection mysql;
+
+    private void Start()
+    {
+        string connectionStr = string.Format("Host={0};Port={1};Username={2};Password={3};Database={4}",
+                                                    ipAdress, port, login, password, dbName);
+
+        mysql = new NpgsqlConnection(connectionStr);
+
+        try
+        {
+            mysql.Open();
+        }
+        catch (NpgsqlException ex)
+        {
+            Debug.LogError(string.Format("ERROR CONNECT: {0}", ex.Message));
+        }
+    }
+
+    public NpgsqlDataReader query(string sql)
+    {
+        NpgsqlCommand command = this.mysql.CreateCommand();
+        command.CommandText = sql;
+
+        return command.ExecuteReader();
+    }
+}
---
+++ Assets/Scripts/SignManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityNpgsql;
+
+public class SignManager : MonoBehaviour
+{
+    public DbManager manager;
+
+    public Text errorBlock;
+
+    public void signIn()
+    {
+        string login = GameObject.Find("emailField").GetComponent<InputField>().text;
+        string pass = GameObject.Find("passField").GetComponent<InputField>().text;
+
+        string sql = string.Format("SELECT uname  FROM users WHERE email='{0}' AND pass='{1}'", login, pass);
+
+        NpgsqlDataReader reader = manager.query(sql);
+
+
+        bool response = false;
+
+        //Debug.Log(reader["uname"]);
+        //PlayerPrefs.SetString("uname", );
+
+        try
+        {
+            reader.Read();
+
+            Debug.Log(reader[0]);
+            PlayerPrefs.SetString("uname", reader[0].ToString());
+
+            response = true;
+        }
+        catch
+        {
+             setError("Логин или пароль не верный");
+        }
+
+        reader.Close();
+
+        if (response == true)
+            SceneManager.LoadScene(1);
+           
+    }
+
+    private void setError(string errorMsg)
+    {
+        errorBlock.text = errorMsg;
+
+        resetLoginForm();
+    }
+
+    private void resetLoginForm()
+    {
+        GameObject.Find("emailField").GetComponent<InputField>().text = string.Empty;
+        GameObject.Find("passField").GetComponent<InputField>().text = string.Empty;
+    }
+}
